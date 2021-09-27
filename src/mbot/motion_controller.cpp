c192c0cb3ed3645af4ffb41cfb3b4c05a0d1037d
mitryand <mitryand@umich.edu>
2021-09-27 15:31:51 -0400

    hrmm

--- src/mbot/motion_controller.cpp
+++ src/mbot/motion_controller.cpp
@@ -85,7 +85,7 @@ private:
     float Kp = 0.25;
     float Ki = 0.0000005;
     float Kd = 40000;
-    float Komega = 0.25;
+    float Komega = 0.5;
     uint64_t t_prev = 0.0;
     uint64_t t_now = 0.0;
     float xDeviation = 0.0;
@@ -122,7 +122,7 @@ public:
         return (fabs(angle_diff(pose.theta, target_heading)) < 0.15); //0.07
     }
 private: 
-    float Kp = 0.25;
+    float Kp = 0.5;
 };
 
 
