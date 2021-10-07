96defb31640395d5c1df8d2be0645910cecd9aee
mitryand <mitryand@umich.edu>
2021-10-07 15:34:13 -0400

    updated python teleop

--- python/mbot_teleop.py
+++ python/mbot_teleop.py
@@ -10,8 +10,8 @@ sys.path.append("lcmtypes")
 import lcm
 from lcmtypes import mbot_motor_command_t
 
-LIN_VEL_CMD = 1.5
-ANG_VEL_CMD = 6.28
+LIN_VEL_CMD = 0.25
+ANG_VEL_CMD = 4
 
 lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=1")
 pygame.init()
