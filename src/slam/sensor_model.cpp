4b605e47590c4184683ab6521dd12b29e93cbcd7
mitryand <mitryand@umich.edu>
2021-10-11 13:37:38 -0400

    magic sauce

--- src/slam/sensor_model.cpp
+++ src/slam/sensor_model.cpp
@@ -32,10 +32,10 @@ double SensorModel::likelihood(const particle_t& sample, const lidar_t& scan, co
             float pastOdds =  map.logOdds(past.x, past.y);
             float beforeOdds = map.logOdds(before.x, before.y);
             if (pastOdds > 0.0) {
-                scanScore += 0.1*pastOdds;
+                scanScore += 0.05*pastOdds;
             }
             if (beforeOdds > 0.0) {
-                scanScore += 0.1*beforeOdds;
+                scanScore += 0.05*beforeOdds;
             }
         }
         
