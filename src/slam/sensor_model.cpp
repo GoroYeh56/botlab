94a5735d5110c82076c0e0a0b391bb99546a0544
mitryand <mitryand@umich.edu>
2021-10-12 14:04:11 -0400

    0.1

--- src/slam/sensor_model.cpp
+++ src/slam/sensor_model.cpp
@@ -32,10 +32,10 @@ double SensorModel::likelihood(const particle_t& sample, const lidar_t& scan, co
             float pastOdds =  map.logOdds(past.x, past.y);
             float beforeOdds = map.logOdds(before.x, before.y);
             if (pastOdds > 0.0) {
-                scanScore += 0.15*pastOdds;
+                scanScore += 0.1*pastOdds;
             }
             if (beforeOdds > 0.0) {
-                scanScore += 0.15*beforeOdds;
+                scanScore += 0.1*beforeOdds;
             }
         }
         
