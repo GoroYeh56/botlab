79d20f23e28796818334d668c57cb8c8ccd3ed35
mitryand <mitryand@umich.edu>
2021-10-12 13:56:38 -0400

    sensact

--- src/slam/action_model.cpp
+++ src/slam/action_model.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 
-ActionModel::ActionModel(void) : k1_(0.4f),k2_(0.01f),initialized_(false)
+ActionModel::ActionModel(void) : k1_(0.3f),k2_(0.01f),initialized_(false)
 {
     //////////////// TODO: Handle any initialization for your ActionModel /////////////////////////
     std::random_device rd;
