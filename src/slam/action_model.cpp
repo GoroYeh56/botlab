11a197cb2600a89d470297e54ca25b01d48ff570
mitryand <mitryand@umich.edu>
2021-10-12 13:16:19 -0400

    acciones

--- src/slam/action_model.cpp
+++ src/slam/action_model.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 
-ActionModel::ActionModel(void) : k1_(0.1f),k2_(0.01f),initialized_(false)
+ActionModel::ActionModel(void) : k1_(0.2f),k2_(0.01f),initialized_(false)
 {
     //////////////// TODO: Handle any initialization for your ActionModel /////////////////////////
     std::random_device rd;
