0cd85e9e1c31b762ab695ca9d7716a49b9f292d0
mitryand <mitryand@umich.edu>
2021-10-21 15:39:25 -0400

    full debug mode yay slow algorithm!

--- src/planning/astar.cpp
+++ src/planning/astar.cpp
@@ -12,71 +12,71 @@ robot_path_t search_for_path(pose_xyt_t start,
     PriorityQueue openList;
     PriorityQueue closedList;
 
-    auto gridGoal = global_position_to_grid_position({ goal.x,goal.y }, distances);
+    auto gridGoal = global_position_to_grid_position({ goal.x, goal.y }, distances);
     Node goalNode(gridGoal.x, gridGoal.y);
 
-    auto gridStart = global_position_to_grid_position({ start.x,start.y }, distances);
+    auto gridStart = global_position_to_grid_position({ start.x, start.y }, distances);
     Node startNode(gridStart.x, gridStart.y);
     startNode.parent = NULL;
     openList.push(startNode);
 
-    //std::cout << "\n-1\n";
+    std::cout << "\n-1\n";
     while (!openList.empty()) {
-        //std::cout << "\n0\n";
-        Node q = openList.pop();
-        //std::cout << "\nq: " << q;
-        std::cout << "\n q: " << q.cell.x << "," << q.cell.y << "\n";
-         //std::cout << "\n1\n";
-        std::vector<Node> kiddos = expand_node(&q, distances, params);
+        std::cout << "\n0\n";
+        Node* q = openList.pop();
+        std::cout << "\nq: " << q;
+        std::cout << "\n q: " << q->cell.x << "," << q->cell.y << "\n";
+        std::cout << "\n1\n";
+        std::vector<Node*> kiddos = expand_node(q, distances, params);
         
         std::cout << "\nkiddos size: " << kiddos.size();
 
         for (int i = 0; i < kiddos.size(); i++) {
-            // std::cout << "\nkiddos.at(i): " << kiddos.at(i);
-            std::cout << "\n kiddos.at(i): " << kiddos.at(i).cell.x << "," << kiddos.at(i).cell.y;
+            std::cout << "\nkiddos.at(i): " << kiddos.at(i);
+            std::cout << "\n kiddos.at(i): " << kiddos.at(i)->cell.x << "," << kiddos->at(i).cell.y;
             
-            if ((kiddos.at(i).cell.x == goalNode.cell.x) && (kiddos.at(i).cell.y == goalNode.cell.y)) {
+            if ((kiddos.at(i)->cell.x == goalNode.cell.x) && (kiddos.at(i)->cell.y == goalNode.cell.y)) {
                 robot_path_t path;
                 path.utime = start.utime;
-                path.path = extract_pose_path(extract_node_path(&kiddos.at(i)),distances);
+                path.path = extract_pose_path(extract_node_path(kiddos.at(i)),distances);
                 path.path_length = path.path.size();
                 std::cout << "\nFOUND PATH!!\n";
                 return path;
             }
             else {
-                // std::cout << "\n2\n";
-               //  std::cout << "\n kiddos.at(i)->g_cost: " << kiddos.at(i)->g_cost;
-                //std::cout << "\n q->g_cost: " << q->g_cost;
-             //    std::cout << "\n g_cost(): " << g_cost(q, kiddos.at(i), distances, params);
-                kiddos.at(i).g_cost = q.g_cost + g_cost(&q, &kiddos.at(i), distances, params);
-                //   std::cout << "\n3\n";
-                kiddos.at(i).h_cost = h_cost(&kiddos.at(i), &goalNode);
-                //    std::cout << "\n4\n";
+                std::cout << "\n2\n";
+                std::cout << "\n kiddos.at(i)->g_cost: " << kiddos.at(i)->g_cost;
+                std::cout << "\n q->g_cost: " << q->g_cost;
+                std::cout << "\n g_cost(): " << g_cost(q, kiddos.at(i), distances, params);
+                kiddos.at(i)->g_cost = q.g_cost + g_cost(&q, kiddos.at(i), distances, params);
+                std::cout << "\n3\n";
+                kiddos.at(i)->h_cost = h_cost(kiddos.at(i), &goalNode);
+                std::cout << "\n4\n";
                 bool skip = false;
                 if ((openList.is_member(kiddos.at(i)))) {
-                    //      std::cout << "\n4.5\n";
-                //     std::cout << "kiddos.at(i)->f_cost: " << kiddos.at(i)->f_cost();
-               //      std::cout << "\n4.6\n";
-                    if (!(openList.get_member(kiddos.at(i))->f_cost() > kiddos.at(i).f_cost())) { // problem child
+                    std::cout << "\n4.5\n";
+                    std::cout << "kiddos.at(i)->f_cost: " << kiddos.at(i)->f_cost();
+                    std::cout << "\n4.6\n";
+                    if (!(openList.get_member(kiddos.at(i))->f_cost() > kiddos.at(i)->f_cost())) { // problem child
                         skip = true;
-                        //        std::cout << "\n6\n";
+                        std::cout << "\n6\n";
                         
                     } 
                 }
                 if ((closedList.is_member(kiddos.at(i)))) {
-                    //         std::cout << "\n7\n";
-                    if (!(closedList.get_member(kiddos.at(i))->f_cost() > kiddos.at(i).f_cost())) {
+                    std::cout << "\n7\n";
+                    if (!(closedList.get_member(kiddos.at(i))->f_cost() > kiddos->at(i).f_cost())) {
                         skip = true;
                     }
                 }
                 if (!skip) {
-                    //        std::cout << "\n8\n";
+                    std::cout << "\n8\n";
                     openList.push(kiddos.at(i));
                 }
                
             }
         }
-        //  std::cout << "\n9\n"; //
+        std::cout << "\n9\n"; //
         closedList.push(q);
     }
 
@@ -108,8 +108,8 @@ double g_cost(Node* from, Node* to, const ObstacleDistanceGrid& distances, const
     return cost;
 }
 
-std::vector<Node> expand_node(Node* node, const ObstacleDistanceGrid& distances, const SearchParams& params) {
-    std::vector<Node> kiddos;
+std::vector<Node*> expand_node(Node* node, const ObstacleDistanceGrid& distances, const SearchParams& params) {
+    std::vector<Node*> kiddos;
     
     // This was super clever I can't believe I never thought of doing this before in this way
     // const int xDeltas[8] = { 1, 1, 1, 0, 0, -1, -1, -1 };
@@ -119,7 +119,7 @@ std::vector<Node> expand_node(Node* node, const ObstacleDistanceGrid& distances,
 
     //add checking for params later!
     for (int i = 0; i < 8; i++) {
-        Node currentKiddo (node->cell.x + xDeltas[i], node->cell.y + yDeltas[i]);
+        Node* currentKiddo (node->cell.x + xDeltas[i], node->cell.y + yDeltas[i]);
         currentKiddo.parent = node;
         
         if (distances.isCellInGrid(currentKiddo.cell.x,currentKiddo.cell.y)) {
@@ -134,22 +134,22 @@ std::vector<Node> expand_node(Node* node, const ObstacleDistanceGrid& distances,
 
 }
 
-std::vector<Node> extract_node_path(Node* node) {
-    std::vector<Node> path;
+std::vector<Node*> extract_node_path(Node* node) {
+    std::vector<Node*> path;
     Node* currentNode = node;
     while (node != NULL) {
-        path.push_back(*node);
+        path.push_back(node);
         node = node->parent;
     }
     return path;
 }
 
 
-std::vector<pose_xyt_t> extract_pose_path(std::vector<Node> nodePath, const ObstacleDistanceGrid& distances) {
+std::vector<pose_xyt_t> extract_pose_path(std::vector<Node*> nodePath, const ObstacleDistanceGrid& distances) {
     std::vector<pose_xyt_t> posePath;
     
     for (int i = 0; i < nodePath.size(); i++) {
-        auto globalPos = grid_position_to_global_position(nodePath.at(i).cell, distances);
+        auto globalPos = grid_position_to_global_position(nodePath.at(i)->cell, distances);
         pose_xyt_t currentPose;
         currentPose.x = globalPos.x;
         currentPose.y = globalPos.y;
--- src/planning/astar.hpp
+++ src/planning/astar.hpp
@@ -28,51 +28,51 @@ struct Node {
 };
 
 struct CompareNode {
-    bool operator() (Node n1, Node n2){
-        if (n1.f_cost() == n2.f_cost()) {
-            return (n1.h_cost) > (n2.h_cost);
+    bool operator() (Node* n1, Node* n2){
+        if (n1->f_cost() == n2->f_cost()) {
+            return (n1->h_cost) > (n2->h_cost);
         }
         else {
-            return (n1.f_cost()) > n2.f_cost();
+            return (n1->f_cost()) > n2->f_cost();
         }
     }
 };
 
 struct PriorityQueue {
-    std::priority_queue<Node, std::vector<Node>, CompareNode> Q;
+    std::priority_queue<Node*, std::vector<Node*>, CompareNode> Q;
     std::vector<Node> elements;
 
     bool empty() {
         return Q.empty();
     }
 
-    bool is_member(Node n) {
+    bool is_member(Node* n) {
         for (auto node : elements) {
-            if (n == node) {
+            if (*n == *node) {
                 return true;
             }
         }
         return false;
     }
 
-    Node* get_member(Node n) {
+    Node* get_member(Node* n) {
         for (auto node : elements) {
-            if (n == node) {
+            if (*n == *node) {
                 return &n;
             }
         }
         return NULL; // nullptr or NULL?
     }
 
-    void push(Node n) {
+    void push(Node* n) {
         elements.push_back(n);
        // std::cout << "\npushing\n";//
         Q.push(n);
     }
 
-    Node pop() {
+    Node* pop() {
         int idx = -1;
-        Node n = Q.top();
+        Node* n = Q.top();
         Q.pop();
         for (int i = 0; i < elements.size(); ++i) {
             if (elements[i] == n) {
@@ -108,9 +108,9 @@ struct SearchParams
 
 double h_cost(Node* from, Node* goal);
 double g_cost(Node* from, Node* to, const ObstacleDistanceGrid& distances, const SearchParams& params);
-std::vector<Node> expand_node(Node* node,const ObstacleDistanceGrid& distances, const SearchParams& params);
-std::vector<Node> extract_node_path(Node* node);
-std::vector<pose_xyt_t> extract_pose_path(std::vector<Node> nodePath, const ObstacleDistanceGrid& distances);
+std::vector<Node*> expand_node(Node* node,const ObstacleDistanceGrid& distances, const SearchParams& params);
+std::vector<Node*> extract_node_path(Node* node);
+std::vector<pose_xyt_t> extract_pose_path(std::vector<Node*> nodePath, const ObstacleDistanceGrid& distances);
 
 
 
