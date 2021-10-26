cfac1ef135a9daaab4be396d523935b652bb9b8a
mitryand <mitryand@umich.edu>
2021-10-26 19:14:08 -0400

    bruh

--- src/slam/particle_filter.cpp
+++ src/slam/particle_filter.cpp
@@ -43,11 +43,12 @@ void ParticleFilter::initializeFilterUniformly(const pose_xyt_t& pose,const Occu
     
     double xLim = (double)map.widthInMeters();
     double yLim = (double)map.heightInMeters();
-    double xStep = (xLim*yLim)/ kNumParticles_;
+    double xStep = (xLim * yLim) / kNumParticles_; //m^2/particle
     double yStep = (xLim * yLim)/ kNumParticles_;
     double xPos = -xLim / 2.0;
     double yPos = -yLim / 2.0;
-
+    std::cout << "\nxLim: " << xLim << "  yLim: " << yLim;
+    std::cout << "\nxStep: " << xStep << "  yStep: " << yStep;
     for (auto& p : posterior_) {
         std::cout << "\nxPos: " << xPos << "  yPos: " << yPos;
         if(xPos<xLim / 2.0) {
