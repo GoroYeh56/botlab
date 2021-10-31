0f58bd39016a9adc9741c72dbbbcfd1f7dc0dff1
mitryand <mitryand@umich.edu>
2021-10-30 23:35:26 -0400

    removed herobrine

--- src/slam/action_model.cpp
+++ src/slam/action_model.cpp
@@ -7,7 +7,7 @@
 
 
 ActionModel::ActionModel(void)
-    : k1_(0.125f) // how much dispersion we have for v and w k1: rotation, k2: translation
+    : k1_(0.135f) // how much dispersion we have for v and w k1: rotation, k2: translation
     , k2_(0.01f) // 
     , initialized_(false)
 {
