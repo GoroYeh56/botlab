97df7c7099913e46854fdcb9d0586c62117ba42f
mitryand <mitryand@umich.edu>
2021-10-30 23:09:28 -0400

    plotting fixed

--- python/plot_slam_true.py
+++ python/plot_slam_true.py
@@ -14,45 +14,57 @@ log = lcm.EventLog(sys.argv[1],"r")
 
 trueData = np.empty((0,4), dtype=float)
 slamData = np.empty((0,4), dtype=float)
-init = 0
+initTrue = 0
+initSlam = 0
+
 for event in log:
     if event.channel == "TRUE_POSE":
         msg = odometry_t.decode(event.data)
-        if init==0:
-            start_utime = msg.utime
-            init = 1
+        if initTrue==0:
+            start_true_utime = msg.utime
+            initTrue = 1
         trueData = np.append(trueData, np.array([[ \
-            (msg.utime-start_utime)/1.0E6, \
+            (msg.utime-start_true_utime)/1.0E6, \
             msg.x, \
             msg.y, \
             msg.theta
             ]]), axis=0)
     if event.channel == "SLAM_POSE":
+        if initSlam==0:
+            start_slam_utime = msg.utime
+            initSlam = 1
         msg = odometry_t.decode(event.data)
-        if init==0:
-            start_utime = msg.utime
-            init = 1
         slamData = np.append(slamData, np.array([[ \
-            (msg.utime-start_utime)/1.0E6, \
+            (msg.utime-start_slam_utime)/1.0E6, \
             msg.x, \
             msg.y, \
             msg.theta
             ]]), axis=0)
 
 errorData = np.empty((0,4), dtype=float)
+j = 0
 for i in range(np.shape(trueData)[0]-1):
-    if(i < np.shape(slamData)[0]):
-        np.append(errorData, np.array([[ \
+    if(j < np.shape(slamData)[0] - 1):
+        if trueData[i,0] > slamData[j,0]:
+            j+=1
+        errorData = np.append(errorData, np.array([[ \
                 trueData[i,0], \
-                trueData[i,1] - slamData[i,1], \
-                trueData[i,2] - slamData[i,2], \
-                trueData[i,3] - slamData[i,3]
+                abs(trueData[i,1] - slamData[j,1]), \
+                abs(trueData[i,2] - slamData[j,2]), \
+                abs(trueData[i,3] - slamData[j,3])
                 ]]), axis=0)
-        print(trueData[i,1] - slamData[i,1])
-#plt.plot(trueData[:,1], trueData[:,2], 'r')
-#plt.plot(slamData[:,1], slamData[:,2], 'r')
+        print(errorData[i,:])
+
+
+fig = plt.figure()
+#plt.plot(trueData[:,0], trueData[:,1] - slamData[:,1], 'r')
 
 plt.plot(errorData[:,0], errorData[:,1], 'r')
-#plt.plot(errorData[:,2], errorData[:,0], 'b')
-#plt.plot(errorData[:,3], errorData[:,0], 'g')
+plt.plot(errorData[:,0], errorData[:,2], 'b')
+#plt.plot(errorData[:,0], errorData[:,3], 'g')
+plt.xlabel("Time (s)")
+plt.ylabel("Error (m)")
+fig.suptitle("SLAM Pose Error versus Time")
+plt.legend("X Error","Y Error")
+
 plt.show()
